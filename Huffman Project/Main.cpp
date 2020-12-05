48f68b0722e2f6b08a1d858e37c8d633eb164815
bLnk <43839740+Soulrealz@users.noreply.github.com>
2020-12-05 17:05:36 +0200

    removed commented dijkstra algorithm from another task

--- Huffman Project/Main.cpp
+++ Huffman Project/Main.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
-#include <string>
+// includes :
+// Table.h = Pair.h, string, vector
+// Pair.h = iostream
+#include "Table.h"
+
+void countChars(std::string& source)
+{
+	// Creating table and filling it with zeroes up to the 126th ascii inclusive
+	Table table;
+	table.fillTable();
+	
+	// Counting each occuring character
+	std::size_t size = source.length();
+	for (std::size_t index = 0; index < size; ++index)
+	{
+		table[(int)source[index]].numberOfOccurances++;
+	}
+
+	// Removing each character with an appearance time equaling 0
+	table.removeZeroes();
+}
 
 
-int main()
+int main(int argc, char* argv[])
 {
 	char symbol;
 	do
@@ -26,7 +45,7 @@ int main()
 		std::string str = "";
 		std::cout << "Enter text to compress:";
 		std::cin >> str;
-		
+		countChars(str);
 	}	
 	break;		
 	case '2':
@@ -43,81 +62,3 @@ int main()
 
 
 
-/*
-#include <cstdio>
-#include <queue>
-#include <vector>
-#include <limits.h>
-
-#define pi pair<int,int>
-
-using namespace std;
-
-int n,m;
-int used[int(1e6)+5], dist[int(1e6)+5], parent[int(1e6)+5];
-
-struct Edge{
-	int to,weight;
-
-	Edge(int to=0,int weight = 0){
-		this->to = to;
-		this->weight = weight;
-	}
-};
-
-vector<Edge>gr[int(1e6)+5];
-
-void init(){
-	int from,to,weight;
-
-	scanf("%d %d",&n,&m);
-
-	for(int i=0;i<m;i++){
-		scanf("%d %d %d",&from,&to,&weight);
-
-		gr[from].push_back({to,weight});
-		gr[to].push_back({from,weight});
-	}
-}
-
-void dijkstra(int start){
-	priority_queue<pi,vector<pi>, greater<pi>>toVisit;///{dist,node}. Dist is first since i want to use the pair comparator
-
-	fill(dist,dist+n,INT_MAX/10);///initialize distances with big numbers
-
-	dist[start] = 0;///distance to start is 0
-	toVisit.push({0,start});///add it to the queue
-	parent[start]=-1;///it is the first node so it has no parent
-
-	while(!toVisit.empty()){
-		int node = toVisit.top().second;///take a node and remove it from the queue
-		toVisit.pop();
-
-		if(used[node])continue;///if it is not the first time we see it just skip
-
-		used[node]=1;///mark it as used
-
-		for(Edge neighbor:gr[node]){///for each of its neighbors
-			if(dist[node] + neighbor.weight >= dist[neighbor.to])continue;///compare the new distance if we use the path trough node with the old distance
-
-			///if it is better update the distance and add to the queue.
-			dist[neighbor.to] = dist[node] + neighbor.weight;
-			parent[neighbor.to] = node;///if the new distance is better, then we use the path trough node to go to its neighbor, so its parent is node.
-			toVisit.push({dist[neighbor.to],neighbor.to});
-		}
-	}
-}
-
-void print(){
-	for(int i=0;i<n;i++){
-		printf("dist to %d is %d\n",i,dist[i]);
-	}
-}
-
-int main(){
-	init();
-	dijkstra(2);
-	print();
-}
-
-*/
