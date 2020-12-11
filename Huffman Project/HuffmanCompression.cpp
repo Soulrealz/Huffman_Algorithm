4e8eeedca3d6168d71c24570fb6488ae1a4aa4a9
bLnk <breekzstalkers@gmail.com>
2020-12-11 16:19:54 +0200

    added func to unify every node and calculate values for huffman tree

--- Huffman Project/HuffmanCompression.cpp
+++ Huffman Project/HuffmanCompression.cpp
@@ -1,6 +1,6 @@
 #include "HuffmanCompression.h"
 
-void Huffman::fillTableWithCharacters(std::string & source)
+void Compression::fillTableWithCharacters(std::string & source)
 {
 	// Counting each occuring character
 	std::size_t size = source.length();
@@ -10,7 +10,7 @@ void Huffman::fillTableWithCharacters(std::string & source)
 	}
 }
 
-void Huffman::arrangeElements()
+void Compression::arrangeElements()
 {
 	// Removing each character with an appearance time equaling 0
 	table.removeZeroes();
@@ -18,11 +18,31 @@ void Huffman::arrangeElements()
 	table.sort();
 }
 
-void Huffman::fillHeap()
+void Compression::fillHeap()
 {
 	std::size_t size = table.size();
 	for (std::size_t i = 0; i < size; i++)
 	{
 		heap.push(Node(table[i], nullptr, nullptr));
 	}
+}
+
+Node Compression::unifyHeap()
+{
+	Node root;
+	while (heap.getSize() != 1)
+	{
+		Node first = heap.top();
+		heap.pop();
+		Node second = heap.top();
+		heap.pop();
+
+		Node newEl = Node(Pair('!', first.data.numberOfOccurances + second.data.numberOfOccurances), &first, &second);
+		heap.push(newEl);
+	}
+	
+	root = heap.top();
+	heap.pop();
+
+	return root;
 }
--- Huffman Project/HuffmanCompression.h
+++ Huffman Project/HuffmanCompression.h
@@ -1,29 +1,39 @@
 #pragma once
-// includes :
+// direct includes :
 // Table.h = Pair.h, string, vector, algorithm
-// Pair.h = iostream
 // MinHeap.h = vector, Node.h
+// HuffmanTree.h = Node.h
+
+// indirect includes
+// Pair.h = iostream
 // Node.h = Pair.h
 #include "Table.h"
 #include "MinHeap.h"
+#include "HuffmanTree.h"
 
-class Huffman
+class Compression
 {
 public:
 	static void fillTableWithCharacters(std::string& source);
 	static void arrangeElements();
 
 	static void fillHeap();
+	
+	static void constructTree() { tree = HuffmanTree(&unifyHeap()); }
 
 	static void printTable() { table.print(); }
 	static void printHeap() { heap.print(); }
 private:
 	static Table table;
 	static MinHeap heap;
+	static HuffmanTree tree;
 
-	Huffman() = default;
-	Huffman(const Huffman& other) = delete;
-	Huffman& operator=(const Huffman& other) = delete;
+	Compression() = default;
+	Compression(const Compression& other) = delete;
+	Compression& operator=(const Compression& other) = delete;
+
+	static Node unifyHeap();
 };
-Table Huffman::table;
-MinHeap Huffman::heap;
+Table Compression::table;
+MinHeap Compression::heap;
+HuffmanTree Compression::tree;
