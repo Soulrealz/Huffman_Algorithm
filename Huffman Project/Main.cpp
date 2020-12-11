bb1de68548c21f7c8024300dd5f542a6afb1da28
bLnk <breekzstalkers@gmail.com>
2020-12-11 16:18:15 +0200

    removed Huffman Class from main to .h and .cpp and renamed it to Compression

--- Huffman Project/Main.cpp
+++ Huffman Project/Main.cpp
@@ -1,58 +1,5 @@
-// includes :
-// Table.h = Pair.h, string, vector, algorithm
-// Pair.h = iostream
-// MinHeap.h = vector, Node.h
-// Node.h = Pair.h
-#include "Table.h"
-#include "MinHeap.h"
+#include "HuffmanCompression.h"
 
-class Huffman
-{
-public:	
-	static void fillTableWithCharacters(std::string& source)
-	{
-		// Counting each occuring character
-		std::size_t size = source.length();
-		for (std::size_t index = 0; index < size; ++index)
-		{
-			table[(int)source[index]].numberOfOccurances++;
-		}		
-	}
-	static void arrangeElements()
-	{
-		// Removing each character with an appearance time equaling 0
-		table.removeZeroes();
-		// Sort by number of occurances
-		table.sort();
-	}
-
-	static void fillHeap()
-	{
-		std::size_t size = table.size();
-		for (std::size_t i = 0; i < size; i++)
-		{
-			heap.push(Node(table[i], nullptr, nullptr));
-		}
-	}
-
-	static void printTable()
-	{		
-		table.print();
-	}
-	static void printHeap()
-	{
-		heap.print();
-	}
-private:
-	 static Table table;
-	 static MinHeap heap;
-
-	 Huffman() = default;
-	 Huffman(const Huffman& other) = delete;
-	 Huffman& operator=(const Huffman& other) = delete;
-};
-Table Huffman::table;
-MinHeap Huffman::heap;
 
 int main(int argc, char* argv[])
 {
@@ -79,11 +26,10 @@ int main(int argc, char* argv[])
 		std::cout << "Enter text to compress: ";
 		std::cin >> str;
 
-		Huffman::fillTableWithCharacters(str);
+		Compression::fillTableWithCharacters(str);
 	}	
 	break;		
 	case '2':
-
 		break;
 	case '3':
 		break;
@@ -91,11 +37,11 @@ int main(int argc, char* argv[])
 		break;
 	}
 		
-	Huffman::arrangeElements();
-	Huffman::printTable();
+	Compression::arrangeElements();
+	Compression::printTable();
 	std::cout << "\n\n\n";
-	Huffman::fillHeap();
-	Huffman::printHeap();
+	Compression::fillHeap();
+	Compression::printHeap();
 
 	return 0;
 }
