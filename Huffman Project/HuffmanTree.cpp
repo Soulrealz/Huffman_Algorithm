5d8e4297911956dcb0b38e4951c84aa7a5a37a46
bLnk <breekzstalkers@gmail.com>
2020-12-29 05:37:46 +0200

    removed copy and wrote destroy properly

--- Huffman Project/HuffmanTree.cpp
+++ Huffman Project/HuffmanTree.cpp
@@ -1,23 +1,12 @@
 #include "HuffmanTree.h"
 
-void HuffmanTree::copy(const Node* source, Node* node)
-{
-	if (!source)
-		return;
-
-	node = new Node(source->data, source->leftNode, source->rightNode);
-	copy(source->leftNode, node->leftNode);
-	copy(source->rightNode, node->rightNode);
-}
-
-void HuffmanTree::destroy(Node* node)
+void HuffmanTree::destroy(Node * node)
 {
 	if (!node)
 		return;
 
 	destroy(node->leftNode);
 	destroy(node->rightNode);
-
 	delete node;
 	node = nullptr;
 }
