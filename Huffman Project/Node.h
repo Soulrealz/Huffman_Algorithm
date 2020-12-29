8eb867f833cf4b9bc9d3b4ef2c3dbc3072b6bd93
bLnk <breekzstalkers@gmail.com>
2020-12-29 05:33:43 +0200

    Shuffled Pair data field to top to see it more easily while debugging lmfao, added op> and op< for node that compares Pair data.occuranceCount also wrote copy ctr

--- Huffman Project/Node.h
+++ Huffman Project/Node.h
@@ -3,13 +3,22 @@
 
 struct Node
 {
+	Pair data;
 	Node* leftNode;
 	Node* rightNode;
-	Pair data;
 
 	Node() : data(Pair()), leftNode(nullptr), rightNode(nullptr) {}
 	Node(const Pair& _data, Node* left, Node* right) : data(_data), leftNode(left), rightNode(right) {}
-	
+	Node(const Node& other) : data(other.data), leftNode(other.leftNode), rightNode(other.rightNode) {}
+
+	bool operator>(const Node& rhs) const
+	{
+		return data.occuranceCount > rhs.data.occuranceCount;
+	}
+	bool operator<(const Node& rhs) const
+	{
+		return data.occuranceCount < rhs.data.occuranceCount;
+	}
 	friend std::ostream& operator<<(std::ostream& out, const Node& element)
 	{
 		out << element.data;
