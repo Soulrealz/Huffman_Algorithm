928b2b7579f1663e42447543c9631b7b6056391d
bLnk <breekzstalkers@gmail.com>
2020-12-29 05:40:09 +0200

    made static HuffmanTree to be pointer and thus wrote destructor to free data afterwards, added comments, added functions to construct and compress the huffman tree

--- Huffman Project/HuffmanCompression.h
+++ Huffman Project/HuffmanCompression.h
@@ -14,26 +14,38 @@
 class Compression
 {
 public:
+	~Compression()
+	{
+		delete tree;
+	}
+
+	// Functions to fill table and sort them whilst removing 0s
 	static void fillTableWithCharacters(std::string& source);
 	static void arrangeElements();
 
+	// Function to fill heap
 	static void fillHeap();
-	
-	static void constructTree() { tree = HuffmanTree(&unifyHeap()); }
 
+	// Functions to construct HuffmanTree and compress text
+	static void constructTree() { tree = new HuffmanTree(&buildTree()); }
+	static void compressTree() { tree->encode(); }
+
+	// Utility Functions to demonstrate progress
 	static void printTable() { table.print(); }
 	static void printHeap() { heap.print(); }
+	static void printTree() { tree->print(); }
+	static void printCompressedTable() { tree->printCodeTable(); }
 private:
 	static Table table;
 	static MinHeap heap;
-	static HuffmanTree tree;
+	static HuffmanTree* tree;
 
 	Compression() = default;
 	Compression(const Compression& other) = delete;
-	Compression& operator=(const Compression& other) = delete;
+	Compression& operator=(const Compression& other) = delete;	
 
-	static Node unifyHeap();
+	// Actual building function for HuffmanTree
+	static Node& buildTree();
 };
-Table Compression::table;
-MinHeap Compression::heap;
-HuffmanTree Compression::tree;
+
+
