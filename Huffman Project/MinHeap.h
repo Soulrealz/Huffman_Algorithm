68523717a6cbcbcf2e3254a37f5a6a2e10b5359c
bLnk <breekzstalkers@gmail.com>
2020-12-29 05:34:38 +0200

    renamed heapify to sort and removed compare func, heapDown is now heapify

--- Huffman Project/MinHeap.h
+++ Huffman Project/MinHeap.h
@@ -6,23 +6,22 @@ class MinHeap
 {
 public:
 	MinHeap() = default;
-	MinHeap(const std::vector<Node>& other) : heapVector(other) { heapify(); }
+	MinHeap(const std::vector<Node>& other) : heapVector(other) { sort(); }
 
 	Node top() const;
 	void pop();
 	void push(const Node& element);
 
 	std::size_t getSize() const { return heapVector.size(); }
-	bool compare(Node& lhs, Node& rhs) { return lhs.data > rhs.data; }
 	void print() const;
 
 	Node& operator[](int index) { return heapVector[index]; }
+	void sort();
 private:
 	std::vector<Node> heapVector;
 
-	void heapDown(const std::size_t& index);
+	void heapify(std::size_t size, const std::size_t& index);
 	void heapUp(const std::size_t& index);
-	void heapify();
 
 	void swap(Node& lhs, Node& rhs);
 };
