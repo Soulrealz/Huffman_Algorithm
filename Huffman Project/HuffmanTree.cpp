43eb8397a06961e9e9ca0a5b60cfddd8302321aa
bLnk <breekzstalkers@gmail.com>
2020-12-30 23:49:25 +0200

    moved encode definition from .h to .cpp, wrote the code needed to save tree for future decompression

--- Huffman Project/HuffmanTree.cpp
+++ Huffman Project/HuffmanTree.cpp
@@ -10,3 +10,58 @@ void HuffmanTree::destroy(Node * node)
 	delete node;
 	node = nullptr;
 }
+
+void HuffmanTree::encode(Node* node, std::string code)
+{
+	if (!node)
+		return;
+	// If there is no left&&right that means element is leaf
+	if (!node->leftNode && !node->rightNode)
+	{
+		map[node->data.symbol] = code;
+		return;
+	}
+
+	if (node->leftNode)
+	{
+		code += '0';
+		encode(node->leftNode, code);
+	}
+	if (node->rightNode)
+	{
+		//popping 1 element to remove the last added '0'
+		code.pop_back();
+		code += '1';
+		encode(node->rightNode, code);
+	}
+}
+
+std::size_t HuffmanTree::saveTreeInformation() const
+{
+	std::string codedTree = "";
+	saveTreeInformation(root, codedTree);
+	std::ofstream outfile("treeInformation.txt", std::ios::out, std::ios::trunc);
+	outfile << codedTree;
+	return codedTree.size();
+}
+void HuffmanTree::saveTreeInformation(Node* node, std::string& string) const
+{
+	if (node == nullptr)
+		return;
+	
+	string += node->data.symbol;
+	string += "(";
+	if (!node->leftNode->leftNode && !node->leftNode->rightNode)
+	{
+		string += node->leftNode->data.symbol;
+	}
+	else saveTreeInformation(node->leftNode, string);
+
+	string += ",";
+	if (!node->rightNode->leftNode && !node->rightNode->rightNode)
+	{
+		string += node->rightNode->data.symbol;
+	}
+	else saveTreeInformation(node->rightNode, string);
+	string += ")";
+}
--- Huffman Project/HuffmanTree.h
+++ Huffman Project/HuffmanTree.h
@@ -2,6 +2,7 @@
 #include "Node.h"
 #include <unordered_map>
 #include <string>
+#include <fstream>
 
 class HuffmanTree
 {
@@ -10,16 +11,13 @@ public:
 	HuffmanTree(Node* source) : root(source) {}
 	~HuffmanTree() { destroy(root); }
 
-	void encode()
-	{
-		encode(root);
-	}
+	void encode() { encode(root); }
+
+	std::unordered_map<char, std::string>& getMap() { return map; }
+	std::size_t saveTreeInformation() const;
 
 	// Print pre compressed tree
-	void print() const
-	{
-		print(root);
-	}
+	void print() const { print(root); }
 	// Print each symbol with its code
 	void printCodeTable() const
 	{
@@ -46,28 +44,6 @@ private:
 		
 
 	//Run in release to force string optimisation below <14 characters.
-	void encode(Node* node, std::string code = "")
-	{
-		if (!node)
-			return;
-		// If there is no left&&right that means element is leaf
-		if (!node->leftNode && !node->rightNode)
-		{
-			map[node->data.symbol] = code;
-			return;
-		}
-
-		if (node->leftNode)
-		{
-			code += '0';
-			encode(node->leftNode, code);
-		}
-		if (node->rightNode)
-		{
-			//popping 1 element to remove the last added '0'
-			code.pop_back();
-			code += '1';
-			encode(node->rightNode, code);
-		}
-	}
+	void encode(Node* node, std::string code = "");
+	void saveTreeInformation(Node* node, std::string& string) const;
 };
